88ad5f3792ccbdc2f30c25f5f27bd692ac485104
Sause <lazureys@gmail.com>
2017-04-14 10:39:26 +0800

    fix no username/password condition

--- server/utils/fetchUtil.js
+++ server/utils/fetchUtil.js
@@ -2,7 +2,11 @@ import request from 'superagent'
 
 import config from '../config'
 
-export const fetcher = path => request.get(`${config.API_SERVER}${path}`)
+export const fetcher = path => (config.USERNAME) ?
+  request.get(`${config.API_SERVER}${path}`)
     .auth(config.USERNAME, config.PASSWORD)
     .then(response => response.body)
     .catch(err => `cannot load data from gocd, error message: ${err}`)
+  : request.get(`${config.API_SERVER}${path}`)
+      .then(response => response.body)
+      .catch(err => `cannot load data from gocd, error message: ${err}`);
